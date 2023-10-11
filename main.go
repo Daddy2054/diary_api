4ac5255f79d6af129fcc4b141cbfa282b439fe77
Jean <acalto2@gmail.com>
2023-10-11 22:39:12 +0200

    route for new entry

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"diary_api/controller"
 	"diary_api/database"
+	"diary_api/middleware"
 	"diary_api/model"
 	"fmt"
 	"log"
@@ -36,7 +37,23 @@ func serveApplication() {
     publicRoutes := router.Group("/auth")
     publicRoutes.POST("/register", controller.Register)
     publicRoutes.POST("/login", controller.Login)
+	
+	protectedRoutes := router.Group("/api")
+    protectedRoutes.Use(middleware.JWTAuthMiddleware())
+    protectedRoutes.POST("/entry", controller.AddEntry)
+    protectedRoutes.GET("/entry", controller.GetAllEntries)
 
     router.Run(":8082")
-    fmt.Println("Server running on port 8000")
+    fmt.Println("Server running on port 8082")
 }
+
+//curl -i -H "Content-Type: application/json" \
+// -X POST \
+// -d '{"username":"<<USERNAME>>", "password":"<<PASSWORD>>"}' \
+// http://localhost:8000/auth/register
+
+
+// curl -d '{"content":"A sample content"}' \
+//     -H "Content-Type: application/json" \
+//     -H "Authorization: Bearer <<JWT>>" \
+//     -X POST http://localhost:8000/api/entry
